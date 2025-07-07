f8508453f0bbe608cf3d190da1afd014d580cf0f
Nikhil Suresh Mohite <nikhilmohite2003@gmail.com>
2025-07-07 21:28:42 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+//import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+//export default defineConfig({
+//
+ // plugins: [react(), tailwindcss()],
+//})
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Replace with your repo name
+const repoName = 'myezz_FoodeDelivery';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: `/${repoName}/`, // 👈 important for GitHub Pages
 })
+
